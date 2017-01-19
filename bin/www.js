49d9bae18b7bc191510a0b8affb66d854c173258
Manit Kwanyun <a@muime.com>
2017-01-19 15:25:22 +0700

    Update to https server

--- bin/www.js
+++ bin/www.js
@@ -1,14 +1,29 @@
 require.main.require('../global');
 
 var app = require(global.modulePath('app')); //Require our app
+var https = require('https');
+//var http = require('http');
+var fs = require('fs');
 var models = require(global.modulePath('', 'model:structured'));
 
 app.set('port', process.env.PORT || 8000);
 
 models.sequelize.sync().then(function () {
-    var server = app.listen(app.get('port'), function() {
+//    // Create an HTTP service.
+//    var server = http.createServer(app).listen(app.get('port'), function() {
+//        console.log('Express server listening on port ' + server.address().port);
+//    });
+
+
+    // Create an HTTPS service identical to the HTTP service.
+    var options = {
+        key: fs.readFileSync('tls/localhost.key'),
+        cert: fs.readFileSync('tls/localhost.crt')
+    };
+    var server = https.createServer(options, app).listen(app.get('port'), function () {
         console.log('Express server listening on port ' + server.address().port);
     });
+
 
     var io = require('socket.io').listen(server);
     io.on('connection', function(socket) {
